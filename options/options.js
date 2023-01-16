5bd4b71a1a9b2114eab6a687ed00bde3cc072f2d
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 07:23:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-morning-dev-57151",
+  url: "https://spring-morning-dev-57151.botics.co",
+  api: "https://spring-morning-dev-57151.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
